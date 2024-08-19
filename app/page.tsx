18409ac716a7576c73e16cac4cac72c29c719b11
Bartek Stopyra <b.stopyra@livechat.com>
2024-08-19 13:12:04 +0200

    initial homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,32 @@
+"use client";
+
+import {
+  Button,
+  Heading,
+  Text,
+} from "@livechat/design-system-react-components";
+import { useRouter } from "next/navigation";
+
 export default function Page() {
-  return <div></div>;
+  const router = useRouter();
+
+  return (
+    <div className="homepage">
+      <div className="homepage-card">
+        <Heading as="div" size="xl">
+          Hi! <span role="img">👋🏻</span>
+        </Heading>
+        <Heading as="div" size="xs" bold>
+          Your app has been successfully installed <span role="img">🎉</span>
+        </Heading>
+        <Text as="div" size="md">
+          This is the apps main page but you can&apos;t find anything
+          interesting here.
+        </Text>
+        <Button kind="primary" onClick={() => router.push("/livechat/details")}>
+          See chat details
+        </Button>
+      </div>
+    </div>
+  );
 }
